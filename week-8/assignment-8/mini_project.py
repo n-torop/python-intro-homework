c0c6815db0a00012efe0931cebe8eec1b71d1851
Natalia <natashalumia@gmail.com>
2026-08-27 19:10:40 -0400

    V5

--- week-8/assignment-8/mini_project.py
+++ week-8/assignment-8/mini_project.py
@@ -1,4 +1,5 @@
 import csv
+
 class ValueMissing(Exception):
     pass
 
@@ -6,11 +7,13 @@ def collect_messy_data(path):
     
     clean_list_dict=[]
     messy_list_dict=[]
+    rows_num=0
 
     try:
         with open (path, "r") as p:
             reader=csv.DictReader(p)
             for index, row in enumerate(reader, start=1):
+                rows_num+=1
                 if None in row:
                     messy_list_dict.append (f"Row {index}: extra column detected — skipped")
                     continue
@@ -27,30 +30,31 @@ def collect_messy_data(path):
                 try:
                     amount=float(dict_item["amount"])
                     clean_list_dict.append(dict_item)
-                      
+                
+
+                except KeyError:
+                    # if name==None or category==None or amount==None:
+                    messy_list_dict.append(f"Row {index}: extra column detected — skipped")
                 except ValueError:
                     
-                    messy_list_dict.append(f"Row {index}: ValueError — could not convert '{dict_item["amount"]}' to float")
-
+                    messy_list_dict.append(f"Row {index}: ValueError — could not convert '{dict_item['amount']}' to float")
+                except ValueMissing:
                     if amount=="" or name=="" or category=="":
                         print ("empty")
                         messy_list_dict.append(f"Row {index}: An expected column is missing from a row.")
-                except KeyError:
-                    if name==None or category==None or amount==None:
-                        messy_list_dict.append(f"Row {index}: extra column detected — skipped")
+                
 
 
     except FileNotFoundError:
         print(f"An error. File not found at '{path}'. Please check the file path.")
         return 
-    return clean_list_dict, messy_list_dict
+    return clean_list_dict, messy_list_dict, rows_num
 
 
-def report(clean, messy):
-    attempted_rows=len(clean)+len(messy)
+def report(clean, messy, attempts):
 
     print ("=== CSV Report ===")
-    print (f"Rows attempted:  {attempted_rows}")
+    print (f"Rows attempted:  {attempts}")
     print (f"Rows parsed:      {len(clean)}")
     print (f"Rows skipped:     {len(messy)}")
     print ("\nSkipped rows:")
@@ -62,9 +66,7 @@ def report(clean, messy):
 
 
 path_name="../data/messy_data.csv"
-
-clean_list_dict, messy_list_dict=collect_messy_data(path_name)
-
-report(clean_list_dict, messy_list_dict)
+clean_list_dict, messy_list_dict, rows_num=collect_messy_data(path_name)
+report(clean_list_dict, messy_list_dict, rows_num)
 
 
