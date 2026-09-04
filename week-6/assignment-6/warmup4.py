278c6cf312634aeb9382e20a46209f114c8909b0
Natalia <natashalumia@gmail.com>
2026-09-04 09:11:09 -0400

    Warmup4

--- week-6/assignment-6/warmup4.py
+++ week-6/assignment-6/warmup4.py
@@ -1,21 +1,22 @@
 def is_valid_score(score):
-    if score>=0 and score<=100:
-        return True
-    elif score<0 or score>100:
-        return False
-    elif score!=int:
+    if type(score)==int:
+        if score>=0 and score<=100:
+            return True
+        elif score<0 or score>100:
+            return False
+    else:
         return False
 
-    
+ 
 try:
     user=int(input("Type your score (0-100): "))
+    ans=is_valid_score(user)  
+    if ans:
+        print ("Valid score.")
+        
+    else:
+        print ("Invalid score — must be between 0 and 100.")
+    
 except ValueError:
     print ("Not an integer.")
-    user=int(input("Type your score (0-100): "))
 
-
-ans=is_valid_score(user)
-if ans:
-    print ("Valid score.")
-else:
-    print ("Invalid score — must be between 0 and 100.")
